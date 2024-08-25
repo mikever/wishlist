3e9386e8ede7af7dc8f8d40bad5a9fcef194a681
Mike Veronie <mveronie@gmail.com>
2024-08-25 15:50:51 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { WishItem } from 'shared/models/wishItem'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'wishlist';
+  items = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find gras that cuts itself'),
+  ]
+  title = 'wishlist'
 }
