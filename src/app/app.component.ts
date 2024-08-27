96366394acb6842be419d3296df648ab5a335abc
Mike Veronie <mveronie@gmail.com>
2024-08-26 23:19:45 -0400

    Drop-down filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,35 @@ import { WishItem } from 'shared/models/wishItem'
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  // TEST: Replace test data
   items: WishItem[] = [
     new WishItem('To Learn Angular'),
     new WishItem('Get Coffee', true),
     new WishItem('Find grass that cuts itself'),
   ]
 
+  listFilter: String = '0'
+
   newWishText = ''
 
   title = 'wishlist'
 
+  visibleItems: WishItem[] = this.items
+
+  // methods
   addNewWish() {
     this.items.push(new WishItem(this.newWishText))
     this.newWishText = ''
+  }
+
+  filterChanged(value: String) {
+    if (value === '0') {
+      this.visibleItems = this.items
+    } else if (value === '1') {
+      this.visibleItems = this.items.filter((item) => !item.isComplete)
+    } else {
+      this.visibleItems = this.items.filter((item) => item.isComplete)
+    }
   }
 
   toggleItem(item: WishItem) {
