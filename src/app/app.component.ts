095a5dc4d5ae07d8a96f9365dfe0c348cc4dc1ce
Mike Veronie <mveronie@gmail.com>
2024-08-31 11:52:59 -0400

    Refactors filter-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core'
 import { WishItem } from 'shared/models/wishItem'
 
-const filters = [
-  (item: WishItem) => item,
-  (item: WishItem) => !item.isComplete,
-  (item: WishItem) => item.isComplete,
-]
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,11 +14,9 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself'),
   ]
 
-  listFilter: any = '0'
-
-  title = 'wishlist'
+  filter: any = () => {}
 
   get visibleItems(): WishItem[] {
-    return this.items.filter(filters[this.listFilter])
+    return this.items.filter(this.filter)
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { WishListComponent } from './wish-list/wish-list.component';
-import { AddWishFormComponent } from './add-wish-form/add-wish-form.component'
+import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
+import { WishFilterComponent } from './wish-filter/wish-filter.component'
 
 @NgModule({
-  declarations: [AppComponent, WishListComponent, AddWishFormComponent],
+  declarations: [AppComponent, WishListComponent, AddWishFormComponent, WishFilterComponent],
   imports: [AppRoutingModule, BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
