a3de511e5c73e6d260ab5ab6ce0807d809b47cf8
Mike Veronie <mveronie@gmail.com>
2024-09-05 23:35:35 -0400

    Adds addWish method in wish service

--- src/app/wish.service.ts
+++ src/app/wish.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
+import { WishItem } from 'shared/models/wishItem'
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +8,32 @@ import { HttpClient } from '@angular/common/http'
 export class WishService {
   constructor(private http: HttpClient) {}
 
+  private getStandardOptions(): any {
+    return {
+      hearder: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    }
+  }
+
   getWishes() {
-    return this.http.get('assets/wishes.json')
+    const options = this.getStandardOptions()
+
+    options.params = new HttpParams({
+      fromObject: {
+        format: 'json',
+      },
+    })
+
+    return this.http.get('assets/wishes.json', options)
+  }
+
+  private addWish(wish: WishItem) {
+    const options = this.getStandardOptions()
+    options.headers = options.headers.set(
+      'Authorization',
+      'value-need-for-authorization',
+    )
+    this.http.post('assets/wishes.json', wish, options)
   }
 }
