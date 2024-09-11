63cc2a20584ad9b0265f644e99706a2bea354555
Mike Veronie <mveronie@gmail.com>
2024-09-11 14:33:18 -0400

    adds validators to form inputs

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
@@ -8,10 +8,17 @@ import { FormControl, FormGroup } from '@angular/forms'
 })
 export class ContactComponent {
   contactForm = new FormGroup({
-    senderName: new FormControl(''),
-    senderEmail: new FormControl(''),
-    senderMessage: new FormControl(''),
+    senderName: new FormControl('', [Validators.required]),
+    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderMessage: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
   })
+
+  emailValidity(senderEmail: any) {
+    return senderEmail?.invalid && (senderEmail?.dirty || senderEmail?.touched)
+  }
 
   submitForm() {
     console.log('~~~ value: ', this.contactForm.valid)
